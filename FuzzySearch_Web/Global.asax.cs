05d04676e933ffab935dc85bf01420fe0e7b7d8a
Tsaregorodtsev <tsaregorodtsev@live.com>
2020-04-19 16:56:50 +0300

    Добавьте файлы проекта.

---
+++ FuzzySearch_Web/Global.asax.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Data.Entity;
+using FuzzySearchDemo.Models;
+
+namespace FuzzySearchDemo
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+
+        // WarAndPeace
+        public string[] WarAndPeace;
+        public int WarAndPeaceNumberOfPages;
+
+        protected void Application_Start()
+        {
+            // WarAndPeace
+            WarAndPeace = Models.SearchInBook_WarAndPeace.СreatePages();
+            WarAndPeaceNumberOfPages = WarAndPeace.Length;
+
+            Application["WarAndPeace"] = WarAndPeace;
+            Application["WarAndPeaceNumberOfPages"] = WarAndPeaceNumberOfPages;
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+        }
+    }
+}
---
+++ FuzzySearch_Web/Models/PageContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace FuzzySearchDemo.Models
+{
+    public class PageContext : DbContext
+    {
+        public DbSet<Page> Pages { get; set; }
+    }
+
+    //public class PageDbInitialaser : DropCreateDatabaseAlways<PageContext>
+    //{
+    //    protected override void Seed(PageContext context)
+    //    {
+
+    //        context.Pages.Add(new Page { Text = "Война и мир" });
+
+    //        base.Seed(context);
+    //    }
+    //}
+}
